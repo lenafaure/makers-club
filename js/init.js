92fda1218527b7735a92d3893cd4c268d1cbea39
Lena Faure <lena.faure@gmail.com>
2016-06-09 11:00:08 +0200

    Initialcommit

---
+++ js/init.js
@@ -0,0 +1,14 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+
+    $(".rotate").textrotator({
+	  animation: "dissolve", // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
+	  separator: ",", // If you don't want commas to be the separator, you can define a new separator (|, &, * etc.) by yourself using this field.
+	  speed: 4000 // How many milliseconds until the next word show.
+	});
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
